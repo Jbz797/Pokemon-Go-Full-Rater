74b5d8de5fdd787fbdb16de162cb5422afd3cc95
root <jibusss50@gmail.com>
2016-08-14 23:40:23 +0200

    add basic moves

---
+++ app/basicController.js
@@ -0,0 +1,51 @@
+(function () {
+	'use strict';
+
+	angular
+		.module('app')
+		.controller('BasicController', BasicController);
+
+	BasicController.$inject = ['$http'];
+
+	/* @ngInject */
+	function BasicController($http) {
+
+		var vm = this;
+
+		activate();
+
+		function activate() {
+
+			$http.get('app/data/basic_moves.json')
+				.then(function (res) {
+					vm.basicMoves = res.data;
+
+					var basicMovesLength = vm.basicMoves.length;
+					var dPSArray = [];
+					var durationArray = [];
+					var nRGArray = [];
+					var nRGPSArray = [];
+					var pwArray = [];
+
+					for(var i = 0; i < basicMovesLength; i++) {
+						dPSArray.push(vm.basicMoves[i].DPS);
+						durationArray.push(vm.basicMoves[i].Duration);
+						nRGArray.push(vm.basicMoves[i].NRG);
+						nRGPSArray.push(vm.basicMoves[i].NRGPS);
+						pwArray.push(vm.basicMoves[i].PW);
+					}
+
+					vm.dPSMax = Math.max.apply(null, dPSArray);
+					vm.dPSMin = Math.min.apply(null, dPSArray);
+					vm.durationMax = Math.max.apply(null, durationArray);
+					vm.durationMin = Math.min.apply(null, durationArray);
+					vm.nRGMax = Math.max.apply(null, nRGArray);
+					vm.nRGMin = Math.min.apply(null, nRGArray);
+					vm.nRGPSMax = Math.max.apply(null, nRGPSArray);
+					vm.nRGPSMin = Math.min.apply(null, nRGPSArray);
+					vm.pwMax = Math.max.apply(null, pwArray);
+					vm.pwMin = Math.min.apply(null, pwArray);
+				});
+		}
+	}
+})();
--- app/chargedController.js
+++ app/chargedController.js
@@ -3,18 +3,19 @@
 
 	angular
 		.module('app')
-		.controller('MainController', MainController);
+		.controller('ChargedController', ChargedController);
 
-	MainController.$inject = ['$http', '$mdDialog'];
+	ChargedController.$inject = ['$http'];
 
 	/* @ngInject */
-	function MainController($http, $mdDialog) {
+	function ChargedController($http) {
 
 		var vm = this;
 
 		activate();
 
 		function activate() {
+
 			$http.get('app/data/charged_moves.json')
 				.then(function (res) {
 					vm.chargedMoves = res.data;
@@ -23,19 +24,15 @@
 					var critArray = [];
 					var dodgeWindowArray = [];
 					var durationArray = [];
-					var healScalarArray = [];
 					var NRGCostArray = [];
 					var pwArray = [];
-					var staminaLossScalarArray = [];
 
 					for(var i = 0; i < chargedMovesLength; i++) {
 						critArray.push(vm.chargedMoves[i].Crit);
 						dodgeWindowArray.push(vm.chargedMoves[i].DodgeWindow);
 						durationArray.push(vm.chargedMoves[i].Duration);
-						healScalarArray.push(vm.chargedMoves[i].HealScalar);
 						NRGCostArray.push(vm.chargedMoves[i].NRGCost);
 						pwArray.push(vm.chargedMoves[i].PW);
-						staminaLossScalarArray.push(vm.chargedMoves[i].StaminaLossScalar);
 					}
 
 					vm.critMax = Math.max.apply(null, critArray);
@@ -44,14 +41,10 @@
 					vm.dodgeWindowMin = Math.min.apply(null, dodgeWindowArray);
 					vm.durationMax = Math.max.apply(null, durationArray);
 					vm.durationMin = Math.min.apply(null, durationArray);
-					vm.healScalarMax = Math.max.apply(null, healScalarArray);
-					vm.healScalarMin = Math.min.apply(null, healScalarArray);
 					vm.NRGCostMax = Math.max.apply(null, NRGCostArray);
 					vm.NRGCostMin = Math.min.apply(null, NRGCostArray);
 					vm.pwMax = Math.max.apply(null, pwArray);
 					vm.pwMin = Math.min.apply(null, pwArray);
-					vm.staminaLossScalarMax = Math.max.apply(null, staminaLossScalarArray);
-					vm.staminaLossScalarMin = Math.min.apply(null, staminaLossScalarArray);
 				});
 		}
 	}
